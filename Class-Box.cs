adc026ef69c3d28163fadee4364ffe5320b48643
MrChandan-Kumar <cksk247@gmail.com>
2021-03-20 09:30:41 +0530

    'Class-Box-Assignment'

---
+++ Class-Box.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace PracticeCSharp
+{   
+    class Box {
+        private int length, breadth, height;
+        Box (this.lenght, this.breadth,height){
+            
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my Second Program Program !");
+        }
+    }
+}
