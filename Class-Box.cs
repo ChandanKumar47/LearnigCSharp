3056fad34bef3bbe254eeb57f8fa56d802af5d11
Chandan Kumar <47926655+ChandanKumar47@users.noreply.github.com>
2021-03-21 15:37:03 +0530

    Update Class-Box.cs

--- Class-Box.cs
+++ Class-Box.cs
@@ -21,10 +21,22 @@ namespace PracticeCSharp
 {   
     class Box {
         private int length, breadth, height;
-        Box (this.lenght, this.breadth,height){
+        //Parametric Constructor
+        Box (int lenght,int breadth,int height){
             this.lenght = lenght;
             this.breadth= breadth;
             this.height= height;
+        }
+        
+        // Copy Constructor
+        Box (Box obj1){
+            this.lenght = obj1.lenght;
+            this.breadth = obj1.breadth;
+            this.height = obj.height;
+        }
+        
+        Public long long int Volume(){
+        return (this.lenght*this*breadth*this.height);
         }
 
     }
@@ -33,7 +45,13 @@ namespace PracticeCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is my Second Program Program !");
+            Console.WriteLine("Enter Length, Breadth and Height of the Box");
+            int lenght = Convert.ToDouble(Console.ReadLine());
+            int breadth = Convert.ToDouble(Console.ReadLine());
+            int height = Convert.ToDouble(Console.ReadLine());
+            Box box = new Box(lenght,breadth,height);
+            Console.WriteLine(box.volume);
+            
         }
     }
 }
